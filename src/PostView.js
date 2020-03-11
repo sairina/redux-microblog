f603309abdaa552401ae4671d47f6f6a06b3bed8
Adam <adamlevitz@gmail.com>
2020-03-11 12:01:09 -0700

    get title list to show link to post view, WIP - post view

--- src/PostView.js
+++ src/PostView.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
+
 
 function PostView(){
+  const { postId } = useParams();
+  
+  console.log(postId)
+
   return(
-    <div>POST VIEW</div>
+    <div>POST VIEW
+    <h2>blog title</h2>
+    <p>description</p>
+    <p>body</p>
+    <button>Edit Form</button>
+    <button>Delete / redirect to home and delete post</button>
+    </div>
   );
 }
 
--- src/TitleList.js
+++ src/TitleList.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function TitleList({ posts }) {
 
   return (
     <div className="TitleList">
+      <Link to={`/${posts.id}`}>
       <h3>{posts.title}</h3>
+      </Link>
       <h4>{posts.description}</h4>
+      
     </div>
   );
 }
